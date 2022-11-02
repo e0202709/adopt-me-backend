c6d998c364053be4bfe5ae71a7add0ad1ed2edc7
e0202709 <e0202709@u.nus.edu>
2022-11-02 11:47:19 +0800

    resolved cors issues and updated apis

--- Routes/pets.js
+++ Routes/pets.js
@@ -1,45 +1,47 @@
 const express = require("express")
 const petsRoutes = express.Router();
 const fs = require('fs');
+var cors = require('cors')
+const dataPath = './Init/allPets.json'
+petsRoutes.use(cors())
 
-const dataPath = './Init/allPets.json' 
-
-// util functions 
+petsRoutes.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
+});
 
 const savePetsData = (data) => {
-    const stringifyData = JSON.stringify(data)
-    fs.writeFileSync(dataPath, stringifyData)
+  const stringifyData = JSON.stringify(data)
+  fs.writeFileSync(dataPath, stringifyData)
+
 }
 
 const getPetsData = () => {
-    const jsonData = fs.readFileSync(dataPath)
-    return JSON.parse(jsonData)    
+  const jsonData = fs.readFileSync(dataPath)
+  return JSON.parse(jsonData)
 }
 
 
-// Retreiving pets data
-petsRoutes.get('/pets', (req, res) => {
-    fs.readFile(dataPath, 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
-
-      res.send(JSON.parse(data));
-    });
-  });
 
 // Create new pets for adoption
 petsRoutes.post('/pets/addpet', (req, res) => {
-   
-    var currentPets = getPetsData()
-    const petId = Math.floor(1000 + Math.random() * 9000)
-   
-    currentPets[petId] = req.body
-     
-    console.log(currentPets);
 
-    savePetsData(currentPets);
-    res.send({success: true, msg: 'new pet added successfully for adoption'})
+  var currentPets = getPetsData()
+  var last = Object.keys(currentPets)[Object.keys(currentPets).length - 1];
+  const petId = parseInt(1) + parseInt(last)
+
+  const rndInt1 = Math.floor(Math.random() * (300 - 200 + 1)) + 200;
+  const rndInt2 = Math.floor(Math.random() * (300 - 200 + 1)) + 200;
+
+  if (req.body.category === "Cat") {
+    req.body.image = `https://placekitten.com/${rndInt1}/${rndInt2}`
+  } else {
+    req.body.image = `https://placedog.net/${rndInt1}/${rndInt2}`
+  }
+  currentPets[petId] = req.body
+  savePetsData(currentPets);
+  res.send({ success: true, msg: 'new pet added successfully for adoption' })
 })
 
 // Get All Pets from json file
@@ -50,8 +52,8 @@ petsRoutes.get('/pets/list', (req, res) => {
 
 // Update - using Put method
 petsRoutes.put('/pets/:id', (req, res) => {
-   var allPets = getPetsData()
-   fs.readFile(dataPath, 'utf8', (err, data) => {
+  var allPets = getPetsData()
+  fs.readFile(dataPath, 'utf8', (err, data) => {
     const petId = req.params['id'];
     allPets[petId] = req.body;
 
@@ -62,16 +64,15 @@ petsRoutes.put('/pets/:id', (req, res) => {
 
 //delete - using delete method
 petsRoutes.delete('/pets/delete/:id', (req, res) => {
-   fs.readFile(dataPath, 'utf8', (err, data) => {
+  fs.readFile(dataPath, 'utf8', (err, data) => {
     var allPets = getPetsData()
 
     const petId = req.params['id'];
 
-    delete allPets[petId];  
+    delete allPets[petId];
     savePetsData(allPets);
     res.send(`Pet Id: ${petId} deleted successfully`)
   }, true);
 })
-
 
 module.exports = petsRoutes
